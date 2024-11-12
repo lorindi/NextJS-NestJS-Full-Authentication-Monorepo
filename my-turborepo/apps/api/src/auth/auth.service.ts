e0e1e043e368b514114109d1a3c6db51a28699ee
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-12 09:28:42 +0200

    Update auth.service.ts

--- my-turborepo/apps/api/src/auth/auth.service.ts
+++ my-turborepo/apps/api/src/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import { CreateUserDto } from './dto/create-user.dto';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  registerUser(createDto: CreateUserDto) {
+    throw new Error('Method no implemented.');
+  }
+}
